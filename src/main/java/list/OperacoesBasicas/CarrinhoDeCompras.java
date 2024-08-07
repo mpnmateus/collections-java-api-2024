51038d6a82d5a425ecaca7b0749ad93fdc905adc
mateusnovo <mpnmateus@gmail.com>
2024-08-07 17:18:54 -0300

    classe carrinho de comprar que implementa as funcionalidades: additem, removeritem, calcular valor total, exibir itens

--- src/main/java/list/OperacoesBasicas/CarrinhoDeCompras.java
+++ src/main/java/list/OperacoesBasicas/CarrinhoDeCompras.java
@@ -9,4 +9,42 @@ public class CarrinhoDeCompras {
     public CarrinhoDeCompras() {
         this.listaItens = new ArrayList<Item>();
     }
+    public void adicionarItem(String nome, double preco, int quantidade){
+        listaItens.add(new Item(nome, preco, quantidade));
+    }
+    public void removerItem(String nome){
+        List<Item> itensParaRemover = new ArrayList<>();
+        for(Item i : listaItens)
+            if(i.getNome().equalsIgnoreCase(nome))
+                itensParaRemover.add(i);
+        listaItens.removeAll(itensParaRemover);
+    }
+    public double calcularValorTotal(){
+        double soma = 0;
+        for (Item i : listaItens)
+            soma += i.getPreco() * i.getQuantidade();
+        return soma;
+    }
+    public void exibirItens(){
+        System.out.println("Temos os seguintes itens: " + listaItens);
+    }
+
+
+    public static void main(String[] args) {
+        CarrinhoDeCompras carrinhoCompras = new CarrinhoDeCompras();
+
+        carrinhoCompras.adicionarItem("Tomate", 5, 2);
+        carrinhoCompras.adicionarItem("Cebola", 5, 5);
+        carrinhoCompras.adicionarItem("Cenoura", 5, 5);
+
+        System.out.println("\nO valor total no carrinho atualmente é: R$" + carrinhoCompras.calcularValorTotal());
+
+        carrinhoCompras.exibirItens();
+
+        carrinhoCompras.removerItem("tomate");
+
+        System.out.println("\nO valor total no carrinho atualmente é: R$" + carrinhoCompras.calcularValorTotal());
+        carrinhoCompras.exibirItens();
+
+    }
 }
