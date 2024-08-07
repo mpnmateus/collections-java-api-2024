e141c7e4752e52aa9883373dc7eefd98b0d23ded
mateusnovo <mpnmateus@gmail.com>
2024-08-07 16:54:17 -0300

    Criando classe CarrinhoDeComras. Ao instanciar um objt desse tipo, automaticamente instanciar um ArrayList vazio

---
+++ src/main/java/list/OperacoesBasicas/CarrinhoDeCompras.java
@@ -0,0 +1,12 @@
+package main.java.list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarrinhoDeCompras {
+    private List<Item> listaItens;
+
+    public CarrinhoDeCompras() {
+        this.listaItens = new ArrayList<Item>();
+    }
+}
