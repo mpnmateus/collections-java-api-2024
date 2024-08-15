828140685c5c44e5c3850fa480472c48331dd803
mateusnovo <mpnmateus@gmail.com>
2024-08-15 20:16:13 -0300

    refatorando bloco de comentario

--- src/main/java/streamAPI/Exercicios/OrdenarOrdemCrescente.java
+++ src/main/java/streamAPI/Exercicios/OrdenarOrdemCrescente.java
@@ -5,16 +5,16 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
 
-//Crie um programa que utilize a Stream API para
-//ordenar a lista de números em ordem crescente e a exiba
-//no console.
+/*
+Crie um programa que utilize a Stream API para
+ordenar a lista de números em ordem crescente e a exiba
+no console.
+*/
 
 public class OrdenarOrdemCrescente {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8,
                 9, 10, 5, 4, 3);
-
-
 
         numeros.stream()
                 .sorted(Comparator.naturalOrder())
