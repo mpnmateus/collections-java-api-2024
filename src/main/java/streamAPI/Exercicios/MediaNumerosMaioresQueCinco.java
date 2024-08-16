432c8247ab87f9060059f4550bc45e7b229ebc1a
mateusnovo <mpnmateus@gmail.com>
2024-08-16 13:53:54 -0300

    refactor: refatorando para tornar mais conciso e eficiente usando streamAPI e lambdas expressions

--- src/main/java/streamAPI/Exercicios/MediaNumerosMaioresQueCinco.java
+++ src/main/java/streamAPI/Exercicios/MediaNumerosMaioresQueCinco.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.BinaryOperator;
 import java.util.function.Predicate;
+import java.util.function.ToIntFunction;
 import java.util.stream.Collectors;
 
 /*
@@ -17,23 +18,34 @@ public class MediaNumerosMaioresQueCinco {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6,
                 7, 8, 9, 10, 10, 4, 3);
 
-        //Predicate<Integer> verificadorMaiorQueCinco = n -> n > 5;
+/*
+        Predicate<Integer> verificadorMaiorQueCinco = n -> n > 5;
 
         List<Integer> listaNumerosMaioresQueCinco =
                 numeros.stream()
                         .filter(n -> n > 5)
                         .toList();
 
-
-
-        //BinaryOperator<Integer> somar = (n1, n2) -> n1 + n2;
+       BinaryOperator<Integer> somar = (n1, n2) -> n1 + n2;
 
         double resultadoSoma = listaNumerosMaioresQueCinco.stream()
                 .reduce(0, (n1, n2) -> n1 + n2);
 
         double media = resultadoSoma / listaNumerosMaioresQueCinco.size();
 
-        System.out.println("A média dos números maiores que 5 da lista é: "
+                System.out.println("A média dos números maiores que 5 da lista é: "
                 + media);
+
+*/
+
+        numeros.stream()
+                        .filter(n -> n > 5)
+                                .mapToInt(n -> n)
+                                        .average()
+                                                .ifPresent(n ->
+                                                        System.out.println("A média " +
+                                                                "dos números maiores que 5 é: " + n));
+
+
     }
 }
